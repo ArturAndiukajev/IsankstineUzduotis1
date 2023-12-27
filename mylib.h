4d15d1180151954c74befb1bc5b794d7189f9069
ArturAndiukajev <144611797+ArturAndiukajev@users.noreply.github.com>
2023-12-27 19:26:28 +0200

    Add files via upload

---
+++ mylib.h
@@ -0,0 +1,14 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <regex>
+#include <string>
+
+using std::string
+using std::map;
+
+
+#endif // MYLIB_H_INCLUDED
